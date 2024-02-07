1c254c204da56642b7e441e2d17deca44ad94a23
Nora Schneider <nora.schneider@ackama.com>
2024-02-08 08:48:40 +1100

    NZSL-74 Upload glossvideos to a temporary directory in retrieval task (#145)
    * Upload glossvideos to a temporary directory in retrieval task
    
    * Cleanup moving glossvideo to valid filepath
    
    ---------
    
    Co-authored-by: Nora Schneider <nora@commoncode.io>

--- signbank/dictionary/tasks.py
+++ signbank/dictionary/tasks.py
@@ -1,3 +1,5 @@
+import os
+from tempfile import TemporaryDirectory
 from typing import TypedDict, List
 from urllib.request import urlretrieve
 
@@ -21,6 +23,16 @@ def move_glossvideo_to_valid_filepath(glossvideo):
     Mimics the rename_file method on GlossVideo without changing the assigned filename to
     include the video_type (would cause issues with uniqueness), but still adds the pk-folder into
     the name.
+
+    rename_file gives the file a new name of the format {glosspk}-{idgloss}_{videotype}_{pk}{ext}.
+    get_valid_name method on the storage class splits the name between gloss_pk and idgloss,
+    then joins it back together as {gloss_pk}/{glosspk}-{idgloss}_{videotype}_{pk}{ext}, and
+    then joins that with glossvideo. So the required end result is
+    glossvideo/{gloss_pk}/{glosspk}-{idgloss}_{videotype}_{pk}{ext}
+
+    In our case we need to give get_valid_name our filename, which at this point looks like
+    /app/media/temp_dir/{glosspk}-{idgloss}_{unique_name}_{pk}{ext}, so we split at / and give
+    get_valid_name only the last bit.
 
     This step is necessary because we create the videos in bulk, and usually the filename and path
     are updated in the save() step.
@@ -53,11 +65,14 @@ def retrieve_videos_for_glosses(video_details: List[VideoDetail]):
     main_video_type = FieldChoice.objects.filter(field="video_type", english_name="main").first()
     videos_to_create = []
 
+    temp_dir = TemporaryDirectory(dir=settings.MEDIA_ROOT)
+
     for video in video_details:
         retrieval_url = f"{settings.NZSL_SHARE_HOSTNAME}{video['url']}"
+        file_name = f"{temp_dir.name}/{video['file_name']}"
         file, _ = urlretrieve(
             retrieval_url,
-            video["file_name"]
+            file_name
         )
         gloss = Gloss.objects.get(pk=video["gloss_pk"])
 
@@ -81,4 +96,5 @@ def retrieve_videos_for_glosses(video_details: List[VideoDetail]):
         )
     GlossVideo.objects.bulk_create(videos_to_create)
 
+    temp_dir.cleanup()
     connection.close()
