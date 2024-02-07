4ada9e7930518439b1ff91eb687f2318edf01a9e
Nora Schneider <nora@commoncode.io>
2024-02-08 10:41:57 +1100

    Revert media root settings for production

--- signbank/settings/production.py
+++ signbank/settings/production.py
@@ -24,7 +24,8 @@ CACHES = {
 }
 
 #: Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+# This is expected to be in a S3 bucket.
+MEDIA_ROOT = '/media'
 # URL that handles the media served from MEDIA_ROOT, used for managing stored files.
 # It must end in a slash if set to a non-empty value.
 MEDIA_URL = '/media/'
