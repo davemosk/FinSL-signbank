d7af5055582fbb5de13820d3c0ead240723f4eb6
jonholdsworth <82071930+jonholdsworth@users.noreply.github.com>
2024-09-03 14:23:07 +1000

    Header refactored

--- bin/get-video-s3-acls.py
+++ bin/get-video-s3-acls.py
@@ -232,11 +232,18 @@ else:
 
 # From the keys present in NZSL, get all their ACL information
 print(f"Getting ACLs for keys from S3 ({AWS_S3_BUCKET}) ...", file=sys.stderr)
+
 # CSV header
-print(
-    f"Video S3 Key{CSV_DELIMITER}Postgres ID{CSV_DELIMITER}Gloss ID{CSV_DELIMITER}"
-    f"Signbank is_public{CSV_DELIMITER}Expected S3 Canned ACL{CSV_DELIMITER}Actual S3 Canned ACL"
-)
+csv_header_list = [
+    "Video S3 Key",
+    "Postgres ID",
+    "Gloss ID",
+    "Signbank Public",
+    "Expected S3 Canned ACL",
+    "Actual S3 Canned ACL",
+]
+print(CSV_DELIMITER.join(csv_header_list))
+
 for video_key, [is_present, db_id, gloss_id, is_public] in all_keys_dict.items():
     canned_acl = ""
     canned_acl_expected = ""
